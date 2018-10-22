d8a5374fd3b0ecc89e86ed9256ef08db2718bee6
wellorange <1045923545@qq.com>
2018-10-22 11:48:32 +0800

    Update minimax1.py

--- arithmetic/minimax/minimax1.py
+++ arithmetic/minimax/minimax1.py
@@ -1,4 +1,11 @@
+import numpy as np
+import string
+import time
+import os
+from collections import defaultdict
 
+def tree(): 
+   return defaultdict(tree)    
 
 
 file_name = 'case/input25.txt'
@@ -11,11 +18,11 @@ number=0
 numbert=1
 temp=[]
 
-
-data=[]     
+  
 exits=[]     # 双方剔除过的
-youkey=[]
 mykey=[]
+youkey=[]
+youexits=[]  # 你剔除过的
 myexits=[]   # 我剔除过的
 for index,line in enumerate(open(file_name)):
     if cur_token==token[0]:
@@ -40,7 +47,7 @@ for index,line in enumerate(open(file_name)):
             temp.append(line.split()[0])
             dataset.append(temp)
             exits.append(line.split()[0])
-            youkey.append(line.split()[0])
+            youkey.append(line.split()[0])   
             number=0
             numbert=1
             temp=[]
@@ -77,22 +84,34 @@ for index,line in enumerate(open(file_name)):
           number=int(line)
           cur_token=token[7]
     elif cur_token==token[7]:
-         
-
          if numbert==number:
             temp.append(line.split()[0])
             dataset.append(temp)
             if line.split()[0][0:5] in mykey:
-                myexits.append(line.split()[0])
+                  myexits.append(line.split()[0])
+            if line.split()[0][0:5] in youkey:
+                  youexits.append(line.split()[0])
+
             temp=[]
             cur_token="null"
             number=0
             numbert=1
          else:
               temp.append(line.split()[0])
+
               if line.split()[0][0:5] in mykey:
                   myexits.append(line.split()[0])
+              if line.split()[0][0:5] in youkey:
+                  youexits.append(line.split()[0])
               numbert+=1
+
+
+
+
+
+
+
+
 
 
 
@@ -100,13 +119,12 @@ for index,line in enumerate(open(file_name)):
 my=[]
 you=[]
 our=[]
-mexi=[]
-yoexi=[]
 record={}
+
 for line in dataset[7]:
     a=0
     for i in line[13:21]:
-        a+=int(i)
+           a+=int(i)
     record[line[0:5]]=a
     if line[0:5] not in exits:
        if (line[10:11]=="N" and line[11:12]=="Y" and line[12:13]=="Y") and (line[5:6]!="F" or int(line[6:9])<=17 or line[9:10]!="N"):
@@ -119,50 +137,100 @@ for line in dataset[7]:
        if line[5:6]=="F" and int(line[6:9])>17 and line[9:10]=="N" and line[10:11]=="N" and line[11:12]=="Y" and line[12:13]=="Y":
           our.append(line)
 
-    if  line[0:5] in mykey:
-        mexi.append(line)
-    if  line[0:5] in youkey:
-        yoexi.append(line)
 
 
+youexitnumber=len(youexits)
+youexittotal=0
+for yii in youexits:
+    for i in yii[13:21]:
+        youexittotal+=int(i)
 
 
+myexitesnumber=len(myexits)
+myexittotal=0
+for mii in myexits:
+    for i in mii[13:21]:
+        myexittotal+=int(i)
+haha=[]
 
-# print(my)
-
-#print("=====")
-
- #   print(my[13:21])
-        #     1             2       3         4         5           6           7           8
-       # print(line[0:5],line[5:6],line[6:9],line[9:10],line[10:11],line[11:12],line[12:13],line[13:21])
-    
+youbit=[]
+mybit=[]
 
 
-print("床位:",dataset[0][0],"  车位:",dataset[1][0])
-print("myexit")
+def Calculateyou(liss,number,n,myrecord,yourecored):
+    if len(liss)==1:
+        yourecored.append(liss[0][0:5])
+        cd=yourecored.copy()
+        youbit.append(cd)
+        yourecored.pop()
+        return liss
+    for inde,lii in enumerate(liss):
+        subblis=liss.copy()
+        subblis.remove(lii)
+        yourecored.append(lii[0:5])
+        if n==10:
+          if len(yourecored)>=3:
+             yourecored=yourecored[0:1]+yourecored[len(yourecored)-1:]
+        Calculatemy(subblis,number,n,myrecord,yourecored)
+
+
+def Calculatemy(lis,number,n,myrecord,yourecord):
+    n-=1
+    for index,li in enumerate(lis):
+        if len(lis)==6:
+            myrecord=[]
+        myrecord.append(li[0:5])
+        if n==9:
+           cc=myrecord.copy()
+           mybit.append(cc)
+           myrecord.pop()
+        elif n==10:
+            if len(myrecord)>=3:
+                 myrecord=myrecord[0:1]+myrecord[len(myrecord)-1:]
+        sublis=lis.copy()
+        sublis.remove(li)
+        Calculateyou(sublis,number,n,myrecord,yourecord)
+Calculatemy(our+my,myexittotal,dataset[1][0]-myexitesnumber,[],[])
+
+# 上面出现的数字是针对case25的,case25跑同之后再替换掉  作用是根据递归的深度来获取路径
 
 
 
 myn=0
-myrecord=0
+mycr=[]
 youn=0
-yourecord=0
-for mmy in mexi:
+youcr=[]
+
+
+
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("                         ")
+print("myexits")
+for mi in myexits:
     a=0
-    for i in mmy[13:21]:
+    for i in mi[13:21]:
         a+=int(i)
-    print(mmy,a)
-print("            ")
-print("youexit")
-for youu in yoexi:
-    a=0
-    for i in youu[13:21]:
-        a+=int(i)
-    print(youu,a)
+    mycr.append(mi[0:5])
+    print(mi,a)
 
 print("            ")
+print("youexits")
+for yi in youexits:
+    a=0
+    for i in yi[13:21]:
+        a+=int(i)
+    youcr.append(yi[0:5])
+    print(yi,a)
+
+print("            ")
+
+
 print("my")
-
 for m in my:
     a=0
     for i in m[13:21]:
@@ -177,35 +245,27 @@ for y in you:
         a+=int(i)
     print(y,a)
 print("            ")
-
-bibi=[]
 print("our:")
 for o in our:
     a=0
     for i in o[13:21]:
         a+=int(i)
-    cc=[]
-    cc.append(o)
-    cc.append(a)
-    bibi.append(cc)
     print(o,a)
 
 
-
-
-def takeSecond(elem):
-    return elem[1]
-# 指定第二个元素排序
-bibi.sort(key=takeSecond,reverse=True)
-iff="my"
-for ii in bibi:
-     if iff=="my":
-         mykey.append(ii[0][0:5])
-         iff="you"
-     else :
-         youkey.append(ii[0][0:5])
-         iff="my"
-
 print(record)
-print(mykey)
-print(youkey)
+for index,yyi in enumerate(mybit):
+    temp=myexittotal
+    for zi in yyi:
+        temp+=int(record[zi])
+
+    if temp>myn:
+        myn=temp
+        mycr=mykey+yyi
+        youcr=youkey+youbit[index]
+
+for ni in youcr:
+    youn+=int(record[ni])
+print("mycore:",myn,"youcore:",youn)
+print("myrecord:",mycr,"yourecord:",youcr)
+
